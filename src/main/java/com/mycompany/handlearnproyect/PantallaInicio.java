ff02fa4ed53bd03dcc820d9d89123a40102e31a8
pared <pared@Guille>
2026-05-19 10:03:01 -0600

    cambio total de la estructura de la interfaz

--- src/main/java/com/mycompany/handlearnproyect/PantallaInicio.java
+++ src/main/java/com/mycompany/handlearnproyect/PantallaInicio.java
@@ -46,7 +46,7 @@ public class PantallaInicio extends JPanel {
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
         
-        JLabel labelHola = new JLabel("¡Hola, Amigo!");
+        JLabel labelHola = new JLabel("¡Hola!, ¿Cómo estas?");
         labelHola.setForeground(new Color(0, 212, 170));
         labelHola.setFont(new Font("Segoe UI", Font.BOLD, 55));
         
